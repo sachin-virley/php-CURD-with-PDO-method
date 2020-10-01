60a4bd129aa7f6fc598c9b3876e020a789d81121
Ramanpreet Singh <50739802+raman3035@users.noreply.github.com>
2020-10-01 05:48:09 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,19 +1,6 @@
 <?php
 
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$name = 'databse_name';
-
-$dsn = "mysql:host=".$host.";dbname=".$name;
-
-try{
-    $pdo = new PDO($dsn,$user,$password);
-}
-catch(PDOException $e){
-    exit("Error :".$e->getMessage());
-}
-
+inlcude("Connection.php");
 $variable = //Enter value;
 $sql = "DELETE FROM table_name WHERE datbase_column = (:value)";
 $query = $pdo->prepare($sql);
